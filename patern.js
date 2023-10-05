82af800595bf709f91ac54452fcad69c4c0500f2
sonalikaneri29 <146074709+sonalikaneri29@users.noreply.github.com>
2023-10-05 14:47:47 +0530

    Add files via upload

---
+++ patern.js
@@ -0,0 +1,141 @@
+
+
+for(i=1; i<=5; i++){
+        console.log("*".repeat(i));
+    }
+    
+
+
+// let i=0
+// while (i<=10) {
+//     console.log(i);
+//     i++
+// }
+
+
+// for of........
+
+// const greeting="hello world"
+// for (const greet of greeting) {
+//     console.log(`each char is ${greet}`)
+// }
+
+
+// map.........
+
+// const map= new map () 
+// map.set('in',"india")
+// map.set('usa',"united of state")
+// map.set('fr',"france")
+// console.log(map)
+
+
+
+
+//maximum num
+// function findMaximum(num1, num2) {
+//     if (num1 > num2) {
+//       return num1;
+//     } else {
+//       return num2;
+//     }
+//   }
+  
+//   const num1 = 10;
+//   const num2 = 20;
+//   const maximum = findMaximum(num1, num2);
+//   console.log("The maximum number is: " + maximum);
+
+
+
+//between tree numbers maximum
+// function findMax(a, b, c) {
+//     if (a >= b && a >= c) {
+//       return a;
+//     } else if (b >= a && b >= c) {
+//       return b;
+//     } else {
+//       return c;
+//     }
+//   }
+  
+//   // Example usage:
+//   const num1 = 10;
+//   const num2 = 25;
+//   const num3 = 15;
+  
+//   const maximum = findMax(num1, num2, num3);
+//   console.log(`The maximum number is: ${maximum}`);
+  
+
+
+
+// Function to check the number's sign
+// function checkNumberSign(number) {
+//     if (number > 0) {
+//       return "Positive";
+//     } else if (number < 0) {
+//       return "Negative";
+//     } else {
+//       return "Zero";
+//     }
+//   }
+  
+//   // Test the function with different numbers
+//   const num1 = 5;
+//   const num2 = -3;
+//   const num3 = 0;
+  
+//   console.log(`${num1} is ${checkNumberSign(num1)}`);
+//   console.log(`${num2} is ${checkNumberSign(num2)}`);
+//   console.log(`${num3} is ${checkNumberSign(num3)}`);
+  
+
+
+
+// Function to check if a number is divisible by 5 and 11
+// function isDivisibleBy5And11(number) {
+//     if (number % 5 === 0 && number % 11 === 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+  
+//   // Test cases
+//   const num1 = 55;
+//   const num2 = 30;
+  
+//   if (isDivisibleBy5And11(num1)) {
+//     console.log(`${num1} is divisible by 5 and 11.`);
+//   } else {
+//     console.log(`${num1} is not divisible by 5 and 11.`);
+//   }
+  
+//   if (isDivisibleBy5And11(num2)) {
+//     console.log(`${num2} is divisible by 5 and 11.`);
+//   } else {
+//     console.log(`${num2} is not divisible by 5 and 11.`);
+//   }
+  
+
+
+// Function to check if a number is even or odd
+// function isEvenOrOdd(number) {
+//     if (number % 2 === 0) {
+//       return "Even";
+//     } else {
+//       return "Odd";
+//     }
+//   }
+  
+//   // Test the function with a number
+//   var num = 8; // Change this to the number you want to check
+//   var result = isEvenOrOdd(num);
+//   console.log(`${num} is ${result}.`);
+  
+
+
+
+
+
